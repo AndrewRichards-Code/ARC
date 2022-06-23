ba117c22bdb5743ff3a50c7750410b3194dc6b5e
Andrew Richards <andyjrichards93@gmail.com>
2022-06-23 14:36:39 +0100

    Added FindPositionInVector().

--- src/Helpers.h
+++ src/Helpers.h
@@ -1,11 +1,18 @@
 #pragma once
+#include <vector>
 
 namespace arc
 {
 	template<typename T>
 	bool FindInVector(const std::vector<T>& container, const T& value)
 	{
-		return std::find(container.begin(), container.end(), value) == container.end();
+		return std::find(container.begin(), container.end(), value) != container.end();
+	}
+
+	template<typename T>
+	std::vector<T>::const_iterator FindPositionInVector(const std::vector<T>& container, const T& value)
+	{
+		return std::find(container.begin(), container.end(), value);
 	}
 
 	template<typename T>
